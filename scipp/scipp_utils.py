e14e04d29fc0be8bbaeb864f9dfab1b65447f250
Neil Vaytet <neil.vaytet@esss.se>
2023-08-24 16:54:08 +0200

    udpate

--- scipp/scipp_utils.py
+++ scipp/scipp_utils.py
@@ -3,6 +3,7 @@
 
 from jupyterquiz import display_quiz
 
+
 questions = {
     "zslice": {
         "question": "I want to get the first `z` slice... which one is it?",
@@ -37,3 +38,27 @@ class QuizLib:
 
 
 quiz = QuizLib(questions)
+
+
+def plot(*x):
+    """
+    Useful plot function for 1d and 2d data
+    """
+    fig, ax = plt.subplots()
+    for a in x:
+        if a.ndim == 1:
+            ax.plot(np.arange(len(a)), a)
+        elif a.ndim == 2:
+            ax.imshow(a, origin="lower")
+
+
+def scatter(x, y):
+    """
+    Simple scatter plot
+    """
+    fig, ax = plt.subplots()
+    ax.scatter(x, y, marker=".", s=1)
+    ax.set_aspect("equal")
+    ax.set_xlim(x.min(), x.max())
+    ax.set_ylim(y.min(), y.max())
+    return ax
