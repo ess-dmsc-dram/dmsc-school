6e2ddf690c68737b8ed2e7b4ef70275c062b93a9
YooSunyoung <17974113+YooSunYoung@users.noreply.github.com>
2025-07-30 17:26:15 +0200

    Fix layout.

--- 6-scicat/notebooks/_scicat_widgets.py
+++ 6-scicat/notebooks/_scicat_widgets.py
@@ -1397,7 +1397,7 @@ class MetadataWidget(widgets.VBox):
         add_button = widgets.Button(
             description="Add Metadata",
             tooltip="Add the metadata from the input fields",
-            layout=Layout(width="108px", margin="5px", height="108px"),
+            layout=Layout(width="108px", margin="5px", height="108px", min_width="108px"),
             style=default_style,
         )
         add_button.button_style = "primary"
